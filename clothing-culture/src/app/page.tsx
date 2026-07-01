d0e0c78ef99663a7784abfb04c5ef29e49a9fbeb
ClothingCulture <clothingculturecart@gmail.com>
2026-07-01 14:45:07 +0000

    initial nextjs setup

---
+++ clothing-culture/src/app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Navbar</h1>
+
+      <h1>Hero</h1>
+
+      <h1>Featured Products</h1>
+
+      <h1>Latest Drops</h1>
+
+      <h1>Best Sellers</h1>
+
+      <h1>Trending</h1>
+
+      <h1>Categories</h1>
+
+      <h1>Footer</h1>
+    </main>
+  );
+}
